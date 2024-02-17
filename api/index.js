e60c13551a6fbf16adf0ecf4a230b0cf8dbf9401
Amrit <amritsinghal2211@gmail.com>
2024-02-17 20:06:54 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
-
+import mongoose from "mongoose"
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config();
 const app = express()
 const port = 3000
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
