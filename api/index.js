389b273498ded782659284a0b28e39c4408ae9b3
Amrit <amritsinghal2211@gmail.com>
2024-02-17 23:09:49 +0530

    Signup api  added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose"
 import express from "express"
 import dotenv from "dotenv"
+import authRouter  from "./routes/auth.route.js"
 dotenv.config();
 const app = express()
+app.use(express.json());
 const port = 3000
 mongoose
   .connect(process.env.MONGO)
@@ -12,11 +14,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  app.use('/api/auth' ,authRouter);
+  app.use((err , req , res , next) =>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error'
+  return res.status(statusCode).json({
+    success : false ,
+    statusCode,
+    message
+  })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  })
