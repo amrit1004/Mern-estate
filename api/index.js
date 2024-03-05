f88650831068a42fb0dffb04bf5d5f3bc0f95ca2
Amrit <amritsinghal2211@gmail.com>
2024-03-05 23:47:23 +0530

    listiing added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express"
 import dotenv from "dotenv"
 import authRouter  from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
+import listingRouter from "./routes/listing.route.js"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 dotenv.config();
@@ -25,6 +26,7 @@ mongoose
   });
   app.use('/api/auth' ,authRouter);
   app.use('/api/user', userRouter);
+  app.use('/api/listing', listingRouter);
   app.use((err , req , res , next) =>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error'
