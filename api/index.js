5e339accce0bbe09df6acd3f955ff20081d6d9de
Amrit <amritsinghal2211@gmail.com>
2024-03-09 19:57:36 +0530

    All Done

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
+import path from 'path'
 dotenv.config();
 const app = express()
 app.use(cors({
@@ -24,9 +25,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+   const __dirname = path.resolve();
   app.use('/api/auth' ,authRouter);
   app.use('/api/user', userRouter);
   app.use('/api/listing', listingRouter);
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
+
+  
   app.use((err , req , res , next) =>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error'
